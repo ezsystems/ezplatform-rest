fabb097f79367155a80817984c68dab4aaacee09
Dawid Parafiński <dawid.parafinski@ez.no>
2019-05-06 14:05:32 +0200

    EZP-28034: Move Common files 1:1

---
+++ eZ/Publish/Core/REST/Common/Tests/Input/Handler/_fixtures/testGeneratorElementList.xml.php
@@ -0,0 +1,19 @@
+<?php
+
+return array(
+  'elementList' =>
+  array(
+    '_media-type' => 'application/vnd.ez.api.elementList+xml',
+    'element' =>
+    array(
+      0 =>
+      array(
+        '_media-type' => 'application/vnd.ez.api.element+xml',
+      ),
+      1 =>
+      array(
+        '_media-type' => 'application/vnd.ez.api.element+xml',
+      ),
+    ),
+  ),
+);
---
+++ eZ/Publish/Core/REST/Common/Tests/Input/Handler/_fixtures/testGeneratorFieldTypeValueList.xml.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+  'Version' =>
+  array(
+    '_media-type' => 'application/vnd.ez.api.Version+xml',
+    'Field' => array(
+      'fieldValue' => array(
+        'Sindelfingen',
+        'Bielefeld',
+        'Skien',
+        'Dinslaken',
+      )
+    )
+  ),
+);
