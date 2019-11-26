4ba6ab9a0459d92e717f58f301f4727497aebf71
Luca Caimi <luca.caimi.lc@gmail.com>
2019-11-26 20:41:47 +0100

    EZP-31122: Added explicitlyHidden flag to Location in REST response (#24)

--- tests/lib/Server/Output/ValueObjectVisitor/RestLocationRootNodeTest.php
+++ tests/lib/Server/Output/ValueObjectVisitor/RestLocationRootNodeTest.php
@@ -33,6 +33,7 @@ class RestLocationRootNodeTest extends RestLocationTest
                     'priority' => 0,
                     'hidden' => false,
                     'invisible' => true,
+                    'explicitlyHidden' => true,
                     'remoteId' => 'remote-id',
                     'parentLocationId' => null,
                     'pathString' => '/1',
