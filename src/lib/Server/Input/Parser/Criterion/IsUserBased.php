07ab2818c4f36cb1bac8c8efc3bb7d4860058083
Maciej Kobus <maciej.kobus@ez.no>
2020-03-12 10:48:27 +0100

    EZP-31080: Fixed handling string true/false as boolean value

--- src/lib/Server/Input/Parser/Criterion/IsUserBased.php
+++ src/lib/Server/Input/Parser/Criterion/IsUserBased.php
@@ -9,18 +9,27 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformRest\Server\Input\Parser\Criterion;
 
 use EzSystems\EzPlatformRest\Input\BaseParser;
+use EzSystems\EzPlatformRest\Input\ParserTools;
 use EzSystems\EzPlatformRest\Input\ParsingDispatcher;
 use EzSystems\EzPlatformRest\Exceptions;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\IsUserBased as IsUserBasedCriterion;
 
 class IsUserBased extends BaseParser
 {
+    /** @var \EzSystems\EzPlatformRest\Input\ParserTools */
+    protected $parserTools;
+
+    public function __construct(ParserTools $parserTools)
+    {
+        $this->parserTools = $parserTools;
+    }
+
     public function parse(array $data, ParsingDispatcher $parsingDispatcher): IsUserBasedCriterion
     {
         if (!array_key_exists('IsUserBasedCriterion', $data)) {
             throw new Exceptions\Parser('Invalid <IsUserBasedCriterion> format');
         }
 
-        return new IsUserBasedCriterion((bool) $data['IsUserBasedCriterion']);
+        return new IsUserBasedCriterion($this->parserTools->parseBooleanValue($data['IsUserBasedCriterion']));
     }
 }
--- src/lib/Server/Input/Parser/Criterion/IsUserEnabled.php
+++ src/lib/Server/Input/Parser/Criterion/IsUserEnabled.php
@@ -9,18 +9,27 @@ declare(strict_types=1);
 namespace EzSystems\EzPlatformRest\Server\Input\Parser\Criterion;
 
 use EzSystems\EzPlatformRest\Input\BaseParser;
+use EzSystems\EzPlatformRest\Input\ParserTools;
 use EzSystems\EzPlatformRest\Input\ParsingDispatcher;
 use EzSystems\EzPlatformRest\Exceptions;
 use eZ\Publish\API\Repository\Values\Content\Query\Criterion\IsUserEnabled as IsUserEnabledCriterion;
 
 class IsUserEnabled extends BaseParser
 {
+    /** @var \EzSystems\EzPlatformRest\Input\ParserTools */
+    protected $parserTools;
+
+    public function __construct(ParserTools $parserTools)
+    {
+        $this->parserTools = $parserTools;
+    }
+
     public function parse(array $data, ParsingDispatcher $parsingDispatcher): IsUserEnabledCriterion
     {
         if (!array_key_exists('IsUserEnabledCriterion', $data)) {
             throw new Exceptions\Parser('Invalid <IsUserEnabledCriterion> format');
         }
 
-        return new IsUserEnabledCriterion((bool) $data['IsUserEnabledCriterion']);
+        return new IsUserEnabledCriterion($this->parserTools->parseBooleanValue($data['IsUserEnabledCriterion']));
     }
 }
