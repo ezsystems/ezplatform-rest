e4ee89655252e75525ceb0499f49bdcd3e90ec00
Adam Wójs <adam.wojs@ez.no>
2021-02-15 11:12:30 +0100

    EZP-32340: Deprecated ezpublish_rest.field_type_processor in favour of ibexa.rest.field_type.processor

--- src/bundle/DependencyInjection/Compiler/FieldTypeProcessorPass.php
+++ src/bundle/DependencyInjection/Compiler/FieldTypeProcessorPass.php
@@ -6,12 +6,17 @@
  */
 namespace EzSystems\EzPlatformRestBundle\DependencyInjection\Compiler;
 
+use eZ\Publish\Core\Base\Container\Compiler\TaggedServiceIdsIterator\BackwardCompatibleIterator;
+use LogicException;
 use Symfony\Component\DependencyInjection\Compiler\CompilerPassInterface;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Reference;
 
 class FieldTypeProcessorPass implements CompilerPassInterface
 {
+    public const FIELD_TYPE_PROCESSOR_SERVICE_TAG = 'ezplatform.field_type.rest.processor';
+    public const DEPRECATED_FIELD_TYPE_PROCESSOR_SERVICE_TAG = 'ezpublish_rest.field_type_processor';
+
     public function process(ContainerBuilder $container)
     {
         if (!$container->hasDefinition('ezpublish_rest.field_type_processor_registry')) {
@@ -20,15 +25,28 @@ class FieldTypeProcessorPass implements CompilerPassInterface
 
         $definition = $container->getDefinition('ezpublish_rest.field_type_processor_registry');
 
-        foreach ($container->findTaggedServiceIds('ezpublish_rest.field_type_processor') as $id => $attributes) {
+        $iterator = new BackwardCompatibleIterator(
+            $container,
+            self::FIELD_TYPE_PROCESSOR_SERVICE_TAG,
+            self::DEPRECATED_FIELD_TYPE_PROCESSOR_SERVICE_TAG
+        );
+
+        foreach ($iterator as $serviceId => $attributes) {
             foreach ($attributes as $attribute) {
                 if (!isset($attribute['alias'])) {
-                    throw new \LogicException('The ezpublish_rest.field_type_processor service tag needs an "alias" attribute to identify the Field Type.');
+                    throw new LogicException(
+                        sprintf(
+                            'Service "%s" tagged with "%s" or "%s" needs an "alias" attribute to identify the Field Type',
+                            $serviceId,
+                            self::FIELD_TYPE_PROCESSOR_SERVICE_TAG,
+                            self::DEPRECATED_FIELD_TYPE_PROCESSOR_SERVICE_TAG
+                        )
+                    );
                 }
 
                 $definition->addMethodCall(
                     'registerProcessor',
-                    [$attribute['alias'], new Reference($id)]
+                    [$attribute['alias'], new Reference($serviceId)]
                 );
             }
         }
