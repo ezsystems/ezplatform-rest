56eaab3d9273ea9b723c391c11f49e17d226ef9b
matx132 <mateusz@mateuszdebinski.pl>
2021-05-20 11:10:40 +0200

    IBX-366: Implemented DateMetadataCriterion to REST API

--- src/lib/Server/Input/Parser/Criterion/DateMetadata.php
+++ src/lib/Server/Input/Parser/Criterion/DateMetadata.php
@@ -8,24 +8,88 @@ namespace EzSystems\EzPlatformRest\Server\Input\Parser\Criterion;
 
 use EzSystems\EzPlatformRest\Input\BaseParser;
 use EzSystems\EzPlatformRest\Input\ParsingDispatcher;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\Operator;
+use eZ\Publish\API\Repository\Values\Content\Query\Criterion\DateMetadata as DateMetadataCriterion;
+use EzSystems\EzPlatformRest\Exceptions;
 
 /**
  * Parser for ViewInput Criterion.
  */
 class DateMetadata extends BaseParser
 {
+    private const OPERATORS = [
+        'IN' => Operator::IN,
+        'EQ' => Operator::EQ,
+        'GT' => Operator::GT,
+        'GTE' => Operator::GTE,
+        'LT' => Operator::LT,
+        'LTE' => Operator::LTE,
+        'BETWEEN' => Operator::BETWEEN,
+    ];
+
     /**
      * Parses input structure to a Criterion object.
      *
-     * @param array $data
-     * @param \EzSystems\EzPlatformRest\Input\ParsingDispatcher $parsingDispatcher
+     * @param string[] $data
      *
      * @throws \EzSystems\EzPlatformRest\Exceptions\Parser
-     *
-     * @return \eZ\Publish\API\Repository\Values\Content\Query\Criterion\DateMetadata
      */
-    public function parse(array $data, ParsingDispatcher $parsingDispatcher)
+    public function parse(array $data, ParsingDispatcher $parsingDispatcher): DateMetadataCriterion
     {
-        throw new \Exception('@todo implement');
+        if (!isset($data['DateMetadataCriterion'])) {
+            throw new Exceptions\Parser('Invalid <DateMetaDataCriterion> format');
+        }
+
+        $dateMetadata = $data['DateMetadataCriterion'];
+
+        if (!isset($dateMetadata['Target'])) {
+            throw new Exceptions\Parser('Invalid <Target> format');
+        }
+
+        $target = strtolower($dateMetadata['Target']);
+
+        if (!in_array($target, DateMetadataCriterion::TARGETS, true)) {
+            throw new Exceptions\Parser('Invalid <Target> format');
+        }
+
+        if (!isset($dateMetadata['Value'])) {
+            throw new Exceptions\Parser('Invalid <Value> format');
+        }
+
+        if (!in_array(gettype($dateMetadata['Value']), ['integer', 'array'], true)) {
+            throw new Exceptions\Parser('Invalid <Value> format');
+        }
+
+        $value = $dateMetadata['Value'];
+
+        if (!isset($dateMetadata['Operator'])) {
+            throw new Exceptions\Parser('Invalid <Operator> format');
+        }
+
+        $operator = $this->getOperator($dateMetadata['Operator']);
+
+        return new DateMetadataCriterion($target, $operator, $value);
+    }
+
+    /**
+     * Get operator for the given literal name.
+     *
+     * For the full list of supported operators:
+     *
+     * @see \eZ\Publish\API\Repository\Values\Content\Query\Criterion\DateMetadata::OPERATORS
+     */
+    private function getOperator(string $operatorName): string
+    {
+        $operatorName = strtoupper($operatorName);
+        if (!isset(self::OPERATORS[$operatorName])) {
+            throw new Exceptions\Parser(
+                sprintf(
+                    'Unexpected DateMetadata operator. Expected one of: %s',
+                    implode(', ', array_keys(self::OPERATORS))
+                )
+            );
+        }
+
+        return self::OPERATORS[$operatorName];
     }
 }
