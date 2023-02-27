e8e9250cf6b63d0c65f9db0292d5f6f11aa78c07
Bartek Wajda <bartlomiej.wajda@ibexa.co>
2023-02-27 18:45:59 +0100

    IBX-5119: Provided more test coverage

--- tests/lib/Server/Output/ValueObjectVisitor/LocationTest.php
+++ tests/lib/Server/Output/ValueObjectVisitor/LocationTest.php
@@ -8,60 +8,75 @@ namespace Ibexa\Tests\Rest\Server\Output\ValueObjectVisitor;
 
 use eZ\Publish\API\Repository\ContentService;
 use eZ\Publish\API\Repository\LocationService;
+use eZ\Publish\API\Repository\PermissionResolver;
 use eZ\Publish\API\Repository\Values\Content\ContentInfo;
 use eZ\Publish\API\Repository\Values\Content\Location as ApiLocation;
+use eZ\Publish\Core\Base\Exceptions\UnauthorizedException;
 use eZ\Publish\Core\Repository\Values\Content\Content;
 use eZ\Publish\Core\Repository\Values\Content\Location;
 use eZ\Publish\Core\Repository\Values\Content\VersionInfo;
 use eZ\Publish\Core\Repository\Values\ContentType\ContentType;
 use EzSystems\EzPlatformRest\Tests\Output\ValueObjectVisitorBaseTest;
 use EzSystems\EzPlatformRest\Server\Output\ValueObjectVisitor;
+use PHPUnit\Framework\MockObject\MockObject;
 
 final class LocationTest extends ValueObjectVisitorBaseTest
 {
-    /** @var \eZ\Publish\API\Repository\LocationService|\PHPUnit\Framework\MockObject\MockObject */
+    private const MAIN_LOCATION_ID = 78;
+
+    private const UNAUTHORIZED_MAIN_LOCATION_ID = 111;
+
+    private const LOCATION_ID = 55;
+
+    /** @var \eZ\Publish\API\Repository\LocationService|MockObject */
     private $locationServiceMock;
 
-    /** @var \eZ\Publish\API\Repository\ContentService|\PHPUnit\Framework\MockObject\MockObject */
+    /** @var ContentService|MockObject */
     private $contentServiceMock;
 
     protected function setUp(): void
     {
+        $this->permissionResolverMock = $this->createMock(PermissionResolver::class);
         $this->locationServiceMock = $this->createMock(LocationService::class);
         $this->contentServiceMock = $this->createMock(ContentService::class);
 
         parent::setUp();
     }
 
-    public function testVisitWithDifferentLocationToMainLocation(): string
-    {
+    /**
+     * @dataProvider providerForMainLocationId
+     */
+    public function testVisitLocationWithDifferentContentMainLocations(
+        ?int $mainLocationId,
+        int $locationId
+    ): void {
         $visitor = $this->getVisitor();
         $generator = $this->getGenerator();
 
         $generator->startDocument(null);
 
+        $contentId = 7;
+        $versionInfo = new VersionInfo();
+
         $location = new Location([
-            'id' => 55,
+            'id' => $locationId,
             'path' => ['1', '25', '42'],
             'priority' => 1,
             'sortField' => ApiLocation::SORT_FIELD_DEPTH,
             'sortOrder' => ApiLocation::SORT_ORDER_ASC,
             'parentLocationId' => 42,
             'contentInfo' => new ContentInfo([
-                'id' => $contentId = 7,
-                'mainLocationId' => $mainLocationId = 78,
+                'id' => $contentId,
+                'mainLocationId' => $mainLocationId,
             ]),
             'content' => new Content([
                 'id' => $contentId,
                 'contentType' => new ContentType(),
-                'versionInfo' => $versionInfo = new VersionInfo(),
+                'versionInfo' => $versionInfo,
             ]),
         ]);
 
-        $this->locationServiceMock->expects(self::once())
-            ->method('loadLocation')
-            ->with($mainLocationId)
-            ->willReturn(new Location(['id' => $mainLocationId]));
+        $this->mockLoadLocation($location);
 
         $this->contentServiceMock->expects(self::once())
             ->method('loadRelations')
@@ -78,14 +93,6 @@ final class LocationTest extends ValueObjectVisitorBaseTest
 
         self::assertNotNull($result);
 
-        return $result;
-    }
-
-    /**
-     * @depends testVisitWithDifferentLocationToMainLocation
-     */
-    public function testResultContainsLocationListElement(string $result): void
-    {
         $this->assertXMLTag(
             [
                 'tag' => 'Location',
@@ -93,21 +100,51 @@ final class LocationTest extends ValueObjectVisitorBaseTest
             $result,
             'Invalid <Location> element.',
         );
-    }
 
-    /**
-     * @depends testVisitWithDifferentLocationToMainLocation
-     */
-    public function testResultContainsLocationAttributes(string $result): void
-    {
         $this->assertXMLTag(
             [
                 'tag' => 'Location',
-                'content' => 55 . 1 . 'false' . 'false',
+                'content' => $location->id . 1 . 'false' . 'false',
             ],
             $result,
             'Invalid <Location> attributes.',
         );
+    }
+
+    private function mockLoadLocation(Location $location): void
+    {
+        $mainLocationId = $location->getContentInfo()->getMainLocationId();
+
+        switch ($mainLocationId) {
+            case $location->id:
+            case null:
+                $this->locationServiceMock->expects(self::never())
+                    ->method('loadLocation');
+                break;
+            case self::UNAUTHORIZED_MAIN_LOCATION_ID:
+                $this->locationServiceMock->expects(self::once())
+                    ->method('loadLocation')
+                    ->with($mainLocationId)
+                    ->willThrowException(new UnauthorizedException('', ''));
+                break;
+            default:
+                $this->locationServiceMock->expects(self::once())
+                    ->method('loadLocation')
+                    ->with($mainLocationId)
+                    ->willReturn(new Location(['id' => $mainLocationId]));
+                break;
+        }
+    }
+
+    public function providerForMainLocationId(): iterable
+    {
+        yield 'same' => [self::MAIN_LOCATION_ID, self::MAIN_LOCATION_ID];
+
+        yield 'empty-main-location' => [null, self::LOCATION_ID];
+
+        yield 'different' => [999, self::LOCATION_ID];
+
+        yield 'unauthorized' => [self::UNAUTHORIZED_MAIN_LOCATION_ID, self::LOCATION_ID];
     }
 
     protected function internalGetVisitor(): ValueObjectVisitor\Location
